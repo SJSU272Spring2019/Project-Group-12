307f28e97abcb73ccb9cbddd380082033a4308c6
renefigueroa <renefigueroa1990@gmail.com>
2019-05-05 22:59:15 -0700

    added probability value in frontend

--- View/CurrentLocation.py
+++ View/CurrentLocation.py
@@ -14,34 +14,37 @@ class CurrentLocation(db.Model):
 	hour = db.Column(db.Float, nullable = False)
 	latitude = db.Column(db.Float, nullable = False)
 	longitude = db.Column(db.Float, nullable = False)
+	probability = db.Column(db.Float, nullable = False)
 
 	def json(self):
-		return{'day': self.day, 'hour': self.hour, 'latitude': self.latitude, 'longitude': self.longitude}
+		return{'day': self.day, 'hour': self.hour, 'latitude': self.latitude, 'longitude': self.longitude, 'probability': self.probability}
 
 	def __repr__(self):
 		current_location_object = {
 			'day':self.day,
 			'hour':self.hour,
 			'latitude':self.latitude,
-			'longitude':self.longitude
+			'longitude':self.longitude,
+			'probability':self.probability
 		}
 		return json.dumps(current_location_object)
 
-	def __init__(self, day, hour, latitude,longitude):
+	def __init__(self, day, hour, latitude,longitude, probability):
 	   self.day = day
 	   self.hour = hour
 	   self.latitude = latitude
 	   self.longitude = longitude
+	   self.probability = probability
 	   locations.append(self)
 
-	def delete_current_parameters(_day,_hour,_latitude,_longitude):
-		is_susccessful = CurrentLocation.query.filter_by(day=_day,hour=_hour, latitude=_latitude,longitude=_longitude).delete()
+	def delete_current_parameters(_day,_hour,_latitude,_longitude,_probability):
+		is_susccessful = CurrentLocation.query.filter_by(day=_day,hour=_hour, latitude=_latitude,longitude=_longitude, probability=_probability).delete()
 		db.session.commit()
 		return bool(is_susccessful)
 
 
-	def add_current_location_parameters(_day, _hour, _latitude, _longitude):
-		new_location = CurrentLocation(day = _day, hour = _hour, latitude = _latitude, longitude = _longitude)
+	def add_current_location_parameters(_day, _hour, _latitude, _longitude,_probability):
+		new_location = CurrentLocation(day = _day, hour = _hour, latitude = _latitude, longitude = _longitude, probability=_probability)
 		db.session.add(new_location)
 		db.session.commit()
 
